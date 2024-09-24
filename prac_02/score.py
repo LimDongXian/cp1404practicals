8a3c21994acfe74cf0633a146d9afb992ae0adf8
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-09-24 10:28:49 +0800

    Converted into function and add random function

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,10 +1,34 @@
-score = float(input("Enter score: "))
-while score > 100 or score < 0:
-    print("Invalid score")
+import random
+
+
+def main():
+    score = get_valid_score()
+    grade = identify_grade(score)
+    print(f"Your score is {score} and it's {grade}")
+    random_score = random_number()
+    random_grade = identify_grade(random_score)
+    print(f"The random score is {random_score} and it's {random_grade}")
+
+
+def get_valid_score():
     score = float(input("Enter score: "))
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    while score > 100 or score < 0:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
+
+
+def identify_grade(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def random_number():
+    return random.randint(1, 100)
+
+
+main()
