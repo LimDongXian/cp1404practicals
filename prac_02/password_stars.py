147a8953017c5ab17a84788cc06a3cc0701fc4f7
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-09-24 10:09:25 +0800

    Final check and added comment for function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,11 +2,13 @@ MINIMUM_LENGTH = 6
 
 
 def main():
+    """Get password and print out by start"""
     password = get_password()
     print(len(password) * "*")
 
 
 def get_password():
+    """Get password input with error check"""
     password = input('Enter password: ')
     while len(password) < MINIMUM_LENGTH:
         print(f"Password should be at least {MINIMUM_LENGTH} characters long.")
