16bd92cb58016813830882014ca7dce3b55655e8
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-09-24 10:49:13 +0800

    Added some more constant and comment for function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,16 +1,19 @@
 MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
 
+MAXIMUM_SCORE = 100
+MINIMUM_SCORE = 0
 EXCELLENT_SCORE = 90
 PASSING_SCORE = 50
 
 
 def main():
+    """Main menu loop to get score, print result, or show stars."""
     score = 0  # added to prevent error when printing result without getting score
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            score = get_valid_score(0, 100, "Enter yours score(0~100): ")
+            score = get_valid_score(MINIMUM_SCORE, MAXIMUM_SCORE, "Enter yours score(0~100): ")
         elif choice == "P":
             grade = identify_grade(score)
             print(f"Your score is {score} and it's {grade}")
@@ -24,6 +27,7 @@ def main():
 
 
 def get_valid_score(low, high, prompt):
+    """Get a valid score between low and high values."""
     score = int(input(prompt))
     while score < low or score > high:
         print("Invalid score")
