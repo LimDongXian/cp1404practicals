c52f38b3497de31f5a352b1bf1ec49dde33559a5
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-09-24 10:06:30 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,17 @@
 MINIMUM_LENGTH = 6
-password = input('Enter password: ')
-while len(password) < MINIMUM_LENGTH:
-    print(f"Password should be at least {MINIMUM_LENGTH} characters long.")
+
+
+def main():
+    password = get_password()
+    print(len(password) * "*")
+
+
+def get_password():
     password = input('Enter password: ')
-print(len(password)* "*")
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Password should be at least {MINIMUM_LENGTH} characters long.")
+        password = input('Enter password: ')
+    return password
+
+
+main()
