3371545c78bfbc2a701393bad39d4fa84ea9121c
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-10-15 10:59:45 +0800

    Added some emails and name into dict

---
+++ prac_05/emails.py
@@ -0,0 +1,9 @@
+"""
+Word Occurrences
+Estimate: 15 minutes
+Actual:   20 minutes
+"""
+
+emails_to_name = {"Lindsay Ward": "lindsay.ward@jcu.edu.au", "Abe": "Abe@gmail.com",
+                  "Jim Boh": "jimbo546@hotmail.com", "Dong Xian": "dongxian.lim@jcu.edu.au"}
+
