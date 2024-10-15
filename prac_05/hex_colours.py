84a3d43dfaf67825d0fb55a873d8d7aac8e119f4
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-10-15 10:23:02 +0800

    Done the user input and code output part

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,3 +1,13 @@
 name_to_code = {"Aqua": "#00ffff", "Bittersweet": "#fe6f5e", "Canary": "#ffff99", "Denim": "#1560bd", "Emerald": "#50c878",
                 "Feldgrau": "#4d5d53", "Ginger": "#b06500", "Heliotrope": "#df73ff", "Iceberg": "#71a6d2", "Jasmine": "#f8de7e", }
-\
+
+
+
+
+colour_name = input("Enter name of the colour: ").title()
+while colour_name != "":
+    try:
+        print(colour_name, "is", name_to_code[colour_name])
+    except KeyError:
+        print("Invalid colour name")
+    colour_name = input("Enter name of the colour: ").title()
