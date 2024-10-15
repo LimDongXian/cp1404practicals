47892564520b65d4ff65abb79fab0965122e2283
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-10-15 10:50:57 +0800

    Reformated first for loop and added another for loop to print output in sorted and reversed sequence. Completed prac 5 word_occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,13 +1,18 @@
 """
 Word Occurrences
 Estimate: 15 minutes
-Actual:   32 minutes
+Actual:   20 minutes
 """
+
+from operator import itemgetter
 
 text = input("Text: ")
 word_to_count = {}
 
 for word in text.split(" "):
     word_to_count[word] = word_to_count.get(word, 0) + 1
-    width = max(len(word) for word in word_to_count.keys())
+
+width = max(len(word) for word in word_to_count.keys())
+
+for word, count in sorted(word_to_count.items(), key=itemgetter(1), reverse=True):
     print(f"{word:{width}} = {word_to_count[word]}")
