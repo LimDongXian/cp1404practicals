84c9034b0409c9568ab63350d9c91397aa3f47df
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-10-15 10:21:09 +0800

    Added 10 dict data of color

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+name_to_code = {"Aqua": "#00ffff", "Bittersweet": "#fe6f5e", "Canary": "#ffff99", "Denim": "#1560bd", "Emerald": "#50c878",
+                "Feldgrau": "#4d5d53", "Ginger": "#b06500", "Heliotrope": "#df73ff", "Iceberg": "#71a6d2", "Jasmine": "#f8de7e", }
+\
