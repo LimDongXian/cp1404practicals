f88877c5fcc04bb6da792829dc01185a15ea3ed5
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-10-28 18:10:55 +0800

    Created limo with some function used

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -14,5 +14,9 @@ def main():
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
+    limo = Car(100)
+    limo.add_fuel(20)
+    print(f"Limo has fuel: {limo.fuel}")
+    limo.drive(115)
 
 main()
