9deec6dae5ec83bea3f68fcbf3427f20a3eb82f5
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-10-29 10:06:18 +0800

    Created a class for guitar with init and str method.

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,3 +1,16 @@
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{name} ({year}) : ${cost}"
+
+
+
+
+
 name = "Gibson L-5 CES"
 year = 1922
 cost = 16035.40
