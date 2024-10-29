a4da2fc4b6c9b1619ae25d3e2608c3920fd75355
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-10-29 10:54:55 +0800

    Format f string in __str__ to show .2f for float Added comment message to every method and class Completed prac_06 guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,16 +2,24 @@ CURRENT_YEAR = 2024
 
 
 class Guitar:
+    """
+    Represents a guitar with specific attributes, including its name, year of manufacture,
+    and cost.
+    """
     def __init__(self, name="", year=0, cost=0):
+        """Initializes a new instance of the Guitar class."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
-        return f"{self.name} ({self.year}) : ${self.cost}"
+        """Returns a string representation of the guitar, showing its name, year, and cost."""
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
 
     def get_age(self):
+        """Calculates the age of the guitar based on the current year."""
         return CURRENT_YEAR - self.year
 
     def is_vintage(self):
+        """Determines if the guitar is considered vintage (older than 50 years)."""
         return Guitar.get_age(self) > 50
