6508cc55c39a6ecd2bfca0210928ffab4ea227ff
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-11-04 17:55:07 +0800

    Modified load_file into a single function Added loop in main to get new guitar Added some print before display guitars

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,6 +2,22 @@ from prac_07.guitar import Guitar
 
 
 def main():
+    guitars = load_file()
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(guitar, "added.\n")
+        name = input("Name: ")
+    guitars.sort()  # sort by year as the Guitar class coded
+    print("\n...snip...\n")
+    print("These are my guitars:")
+    display_guitars(guitars)
+
+
+def load_file():
     guitars = []
     in_file = open("guitars.csv", "r")
     for line in in_file:
@@ -9,8 +25,7 @@ def main():
         guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
         guitars.append(guitar)
     in_file.close()
-    guitars.sort()  # sort by year as the Guitar class coded
-    display_guitars(guitars)
+    return guitars
 
 
 def display_guitars(guitars):
