d72982dc4d41b04720df69abdf208ccc23822d5a
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-11-05 10:48:11 +0800

    Added checking for complete function and li method for sorting

--- prac_07/project.py
+++ prac_07/project.py
@@ -9,6 +9,12 @@ class Project:
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
 
+    def __lt__(self, other):
+        return self.priority < other.priority
+
     def __str__(self):
         return (f"{self.name}, start: {self.start_data}, priority {self.priority}, "
                 f"estimate: ${self.cost_estimate}, completion: {self.completion_percentage} %")
+
+    def is_completed(self):
+        return self.completion_percentage == 100
