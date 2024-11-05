5dd1bd4c028c159c265f0276a05bd486c46e76b2
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-11-05 12:01:25 +0800

    Added project update feature and save functionality, with validation for project input.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -9,6 +9,9 @@ from prac_07.project import Project
 MENU = (" - (L)oad projects\n - (S)ave projects\n - (D)isplay projects\n - (F)ilter projects by dat\n"
         " - (A)dd new project\n - (U)pdate project\n - (Q)uit")
 FILENAME = "projects.txt"
+PERCENTAGE_THRESHOLD = 100
+PRIORITY_THRESHOLD = 10
+MINIMUM_NUMBER = 0
 
 
 def main():
@@ -26,12 +29,12 @@ def main():
         elif choice == "D":
             display_projects(projects)
         elif choice == "F":
-            print("Filter projects by dat")
+            print("Filter projects by date")
         elif choice == "A":
             print("Let's add a new project")
             add_project(projects)
         elif choice == "U":
-            print("Update project")
+            update_project(projects)
         else:
             print("Invalid Choice")
         print(MENU)
@@ -48,6 +51,13 @@ def load_file(filename):
             project = Project(str(part[0]), part[1], int(part[2]), float(part[3]), int(part[4]))
             projects.append(project)
         return projects
+
+
+def save_file(projects):
+    with open(FILENAME, 'w') as out_file:
+        for project in projects:
+            out_file.write(f"{project.name}, {project.start_data}, {project.priority}"
+                           f", {project.cost_estimate}, {project.completion_percentage}\n")
 
 
 def display_projects(projects):
@@ -69,6 +79,28 @@ def add_project(projects):
     completion_percentage = int(input("Percent complete: "))
     project = Project(name, start_date, priority, cost_estimate, completion_percentage)
     projects.append(project)
+
+
+def update_project(projects):
+    for i, project in enumerate(projects):
+        print(f"{i} {project}")
+    project_choice = get_valid_number("Project choice: ", len(projects) + 1, MINIMUM_NUMBER)
+    selected_project = projects[project_choice]
+    print(selected_project)
+    new_percentage = get_valid_number("New percentage: ", PERCENTAGE_THRESHOLD, MINIMUM_NUMBER)
+    new_priority = get_valid_number("New priority: ", PRIORITY_THRESHOLD, MINIMUM_NUMBER)
+    selected_project.update_project(new_priority, new_percentage)
+
+
+def get_valid_number(prompt, high, low):
+    try:
+        number = int(input(prompt))
+        if number < low or number > high:
+            print("Invalid number")
+            number = int(input(prompt))
+    except ValueError:
+        return ""
+    return number
 
 
 main()
