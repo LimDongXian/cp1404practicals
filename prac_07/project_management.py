e2d4e8faf6becf540c92cf17ca0a77fabb6aea43
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-11-05 11:07:23 +0800

    Added functionality to load projects from a user-specified file and implemented an 'Add Project' feature. Updated the main menu to accommodate new functionality, including handling project details input.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,39 +3,43 @@ Project Management
 Estimate: 30 minutes
 Actual:    minutes
 """
+import datetime
 from prac_07.project import Project
 
-MENU = ("- (L)oad projects\n - (S)ave projects\n - (D)isplay projects\n - (F)ilter projects by dat\n"
+MENU = (" - (L)oad projects\n - (S)ave projects\n - (D)isplay projects\n - (F)ilter projects by dat\n"
         " - (A)dd new project\n - (U)pdate project\n - (Q)uit")
 FILENAME = "projects.txt"
 
 
 def main():
-    projects = load_file()
+    projects = load_file(FILENAME)
     print("Welcome to Pythonic Project Management")
     print(f"Loaded {len(projects)} projects from {FILENAME}")
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            print("Loaded projects")
-        if choice == "S":
+            filename = input("Enter filename to load projects: ")
+            projects = load_file(filename)
+        elif choice == "S":
             print("Save projects")
-        if choice == "D":
+        elif choice == "D":
             display_projects(projects)
-        if choice == "F":
+        elif choice == "F":
             print("Filter projects by dat")
-        if choice == "A":
-            print("Add new project")
-        if choice == "U":
+        elif choice == "A":
+            print("Let's add a new project")
+            add_project(projects)
+        elif choice == "U":
             print("Update project")
         else:
             print("Invalid Choice")
-            choice = input(">>> ").upper()
+        print(MENU)
+        choice = input(">>> ").upper()
     print("Thank you for using custom-built project management software.")
 
 
-def load_file():
+def load_file(filename):
     projects = []
     with open(FILENAME, 'r') as in_file:
         in_file.readline()
@@ -55,6 +59,16 @@ def display_projects(projects):
     print("Completed projects:")
     for project in sorted(completed_projects):
         print(project)
+
+
+def add_project(projects):
+    name = input("Name: ")
+    start_date = input("Start date (dd/mm/yyyy): ")
+    priority = int(input("Priority: "))
+    cost_estimate = float(input("Cost estimate: $"))
+    completion_percentage = int(input("Percent complete: "))
+    project = Project(name, start_date, priority, cost_estimate, completion_percentage)
+    projects.append(project)
 
 
 main()
