1290d677e1b50e01b2e0ab29bc8415ba3be2dbff
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-11-05 10:47:45 +0800

    Changed project as each part has their own type Updated display as it print incomplete and complete project seperatly

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -18,13 +18,19 @@ def load_file():
         in_file.readline()
         for line in in_file:
             part = line.strip().split("	")
-            project = Project(part[0], part[1], part[2], part[3], part[4])
+            project = Project(str(part[0]), part[1], int(part[2]), float(part[3]), int(part[4]))
             projects.append(project)
         return projects
 
 
 def display_projects(projects):
-    for project in projects:
+    incomplete_projects = [project for project in projects if not project.is_completed()]
+    completed_projects = [project for project in projects if project.is_completed()]
+    print("Incomplete projects:")
+    for project in sorted(incomplete_projects):
+        print(project)
+    print("Completed projects:")
+    for project in sorted(completed_projects):
         print(project)
 
 
