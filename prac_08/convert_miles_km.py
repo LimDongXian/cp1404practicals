b00aaad8d43357998fc0b481b74031dbede46c47
LimDongXian <dongxian.lim@my.jcu.edu.au>
2024-11-12 21:45:13 +0800

    Added StringProperty, added input function, added on press to calculate mile convert, added fucntion to button up and down

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,12 +1,26 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
 
 
 class ConvertMilesKm(App):
+    output_km = StringProperty()
+
     def build(self):
         self.title = "Convert Miles to Km"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_calculation(self):
+        value = float(self.root.ids.input_miles.text)
+        result = value * MILES_TO_KM
+        self.root.ids.output_km.text = str(result)
+
+    def handle_increment(self, modify):
+        new_value = float(self.root.ids.input_miles.text) + modify
+        self.root.ids.input_miles.text = str(new_value)
 
 
 ConvertMilesKm().run()
