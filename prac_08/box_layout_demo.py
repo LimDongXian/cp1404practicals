d6a6de9c82793399bc92167d19b42c97294d6227
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-11-13 10:37:16 +0800

    Added docstring comment for every class and functions/methods Completed prac_08 box_layout_demo.py

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,17 +3,21 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Main application class for demonstrating Box Layout."""
     def build(self):
+        """Build the application by loading the .kv layout file."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Update the output label with a greeting message."""
         # print("test")
-        self.root.ids.output_label.text = "Hello "
+        # self.root.ids.output_label.text = "Hello "
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clear the input text and output label."""
         # print("clear")  # for testing use
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
