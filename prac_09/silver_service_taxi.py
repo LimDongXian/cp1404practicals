5f44281ede624654413010dfa09d017b45ef9824
Lim Dong Xian <dongxian.lim@my.jcu.edu.ai>
2024-11-19 10:23:13 +0800

    Done child class and ready for testing

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,9 +2,12 @@ from prac_09.taxi import Taxi
 
 
 class SliverServiceTaxi(Taxi):
-    flag_fall = 4.5
+    flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * self.fanciness
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
